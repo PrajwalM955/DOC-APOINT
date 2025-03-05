e3351b55a5a6bb0521cfc313d82b87ffb0dee2c4
Prajwal.M <23bcaf47@kristujayanti.com>
2025-03-05 15:37:53 +0530

    commit1

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -112,7 +112,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(416, 434)
+        Me.ClientSize = New System.Drawing.Size(424, 440)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
